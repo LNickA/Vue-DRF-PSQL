c1ffba5511d2039042cd4ca5c91d8e2866c9849d
LNickA <104496917+LNickA@users.noreply.github.com>
2022-11-02 16:18:47 +0200

    up

--- backend/restapi/restapi/settings.py
+++ backend/restapi/restapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pl5y@9r*f65#)!*6)p)g+0&ow%-xbh+jrc#_%b(nxkcm4a*9!_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
