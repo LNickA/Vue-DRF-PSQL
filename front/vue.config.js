67f7f1bf6e752ef15c01ccb03d68b90724e1505f
LNickA <104496917+LNickA@users.noreply.github.com>
2022-11-07 20:44:29 +0200

    Update vue.config.js

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,11 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require("path");
 
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  chainWebpack(config) {
-    config.resolve.symlinks(false);
-    config.resolve.alias.set("vue", path.resolve("./node_modules/vue"));
-  },
+
 })
